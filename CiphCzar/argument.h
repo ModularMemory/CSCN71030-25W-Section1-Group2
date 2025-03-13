b190bdbe9b2491aebf37b330869ca88498fe746b
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-03-13 14:42:52 -0400

    Fix compiler errors

--- CiphCzar/argument.h
+++ CiphCzar/argument.h
@@ -10,18 +10,18 @@ typedef enum {
     FLOAT_ARG
 } argument_type_t;
 
+typedef union {
+    int integer;
+    float fp;
+    char* string;
+} argument_union_t;
+
 typedef struct argument {
     struct argument* next;
     const char* description;
     argument_type_t arg_type;
     argument_union_t arg_union;
 } argument_t, *pargument_t;
-
-typedef union {
-    int integer;
-    float fp;
-    char* string;
-} argument_union_t;
 
 result_t append_integer_arg(pargument_t* list, const char* description, int initial_val);
 
