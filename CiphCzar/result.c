eca37e9b3aaada7232342a1b7feeb98d9f737b19
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-03-13 14:20:16 -0400

    Rework results, arguments, algorithms

--- CiphCzar/result.c
+++ CiphCzar/result.c
@@ -2,10 +2,10 @@
 
 #include "result.h"
 
-result_t result_ok(void) {
-    return (result_t) { .success = true, .message = "Ok" };
+result_t result_ok(void* data) {
+    return (result_t) { .success = true, .data = data, .message = "Ok" };
 }
 
 result_t result_error(const char* message) {
-    return (result_t) { .success = false, .message = message };
+    return (result_t) { .success = false, .data = NULL, .message = message };
 }
--- CiphCzar/result.h
+++ CiphCzar/result.h
@@ -4,9 +4,16 @@
 
 typedef struct {
     bool success;
+    void* data;
     const char* message;
 } result_t;
 
-result_t result_ok(void);
+/// <summary>
+/// Creates a success result with optional data
+/// </summary>
+result_t result_ok(void* data);
 
+/// <summary>
+/// Creates a non-success result with a message
+/// </summary>
 result_t result_error(const char* message);
