719b7cf22730360cbe4e8a87613a8c0cd7dfbeb3
Iris H <ihahn2449@conestogac.on.ca>
2025-03-18 14:48:16 -0400

    finished read_recipe function (hopefully)

--- CiphCzar/fileIO.c
+++ CiphCzar/fileIO.c
@@ -69,55 +69,52 @@ status_t write_data(const char* filename, data_t data) {
 }
 
 
-// The save file will look something like: 
-// 
-// algorithm_name algorithm_name algorithm_name etc.. \n
-// arg arg arg arg etc EOF
-// 
-// reading the file will first get all the algorithms by reading characters one by one until a space, then using get_algorithm_by_name
-// to convert it to an algorithm struct which gets saved in the recipe linked list, it does it until it hits a \n then it starts reading
-// for arguments 
+
 status_t read_recipe(const char* filename, recipe_t* recipe) {
 	FILE* fp = fopen(filename, "r");
 	if (fp == NULL) {
 		return status_error("File not found");
 	}
+	int count;
+	status_t res_count = read_int_from_stream(fp, &count);
+	int i = 0;
+	while(i < count) {
+		char** alg_name;
+		read_string_from_stream(fp, alg_name);
 
-	char** alg_name;
-	read_string_from_stream(fp, alg_name);
-
-	result_t alg_res = get_algorithm_by_name(*alg_name);
-	if (!alg_res.success) {
-		return status_error("Failed to load algorithm");
-	}
-		
-	algorithm_t alg;
-	clone_algorithm(*((algorithm_t*)alg_res.data), &alg);
-
-	pargument_t current = alg.additional_args;
-	while (current) {
-		switch (current->arg_type) {
-		case INTEGER_ARG:
-			int data_int;
-			status_t res_int = read_int_from_stream(fp, &data_int);
-			current->arg_union.integer = data_int;
-		case FLOAT_ARG:
-			float data_flt;
-			status_t res_flt = read_float_from_stream(fp, &data_flt);
-			current->arg_union.integer = data_flt;
-		case STRING_ARG:
-			char** data_str;
-			status_t res_str = read_string_from_stream(fp, data_str);
-			current->arg_union.string = *data_str;
+		result_t alg_res = get_algorithm_by_name(*alg_name);
+		if (!alg_res.success) {
+			return status_error("Failed to load algorithm");
 		}
-				current = current->next;
+			
+		algorithm_t alg;
+		clone_algorithm(*((algorithm_t*)alg_res.data), &alg);
+
+		pargument_t current = alg.additional_args;
+		while (current) {
+			switch (current->arg_type) {
+			case INTEGER_ARG:
+				int data_int;
+				status_t res_int = read_int_from_stream(fp, &data_int);
+				current->arg_union.integer = data_int;
+			case FLOAT_ARG:
+				float data_flt;
+				status_t res_flt = read_float_from_stream(fp, &data_flt);
+				current->arg_union.integer = data_flt;
+			case STRING_ARG:
+				char** data_str;
+				status_t res_str = read_string_from_stream(fp, data_str);
+				current->arg_union.string = *data_str;
+			}
+					current = current->next;
+		}
+		status_t validation = alg.validate_args(alg.additional_args);
+		if (!validation.success) {
+			fprintf(stderr, "Failed to validate args for %s. Using default values.", alg.name);
+			alg.reset_args(&alg);	
+		}
+		i++;
 	}
-	status_t validation = alg.validate_args(alg.additional_args);
-	if (!validation.success) {
-		fprintf(stderr, "Failed to validate args for %s. Using default values.", alg.name);
-		alg.reset_args(&alg);	
-	}
-		
 	fclose(fp);
 	return status_ok();
 }
