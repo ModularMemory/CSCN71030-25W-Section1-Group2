8ab92122121ea53b4845fa22801d912a4d66e2a2
Cygnus <ari.bea.kess@gmail.com>
2025-03-18 14:34:48 -0400

    minor typo fix

--- CiphCzar/to_decimal.h
+++ CiphCzar/to_decimal.h
@@ -10,7 +10,7 @@ void to_decimal_reset_args(algorithm_t* alg);
 
 #define CREATE_ALG_TO_DECIMAL(void)\
 {\
-    .name = "To Deciaml",\
+    .name = "To Decimal",\
     .description = "Converts the input buffer into a string of decimal codes.",\
     .execute = to_decimal_execute,\
     .validate_args = to_decimal_validate_args,\
