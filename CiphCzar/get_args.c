d6b3a15bc72fc42aa086e5e84146ae246b13f173
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-03-18 14:49:13 -0400

    Revamp algorithm argument fetching

--- CiphCzar/get_args.c
+++ CiphCzar/get_args.c
@@ -3,70 +3,71 @@
 
 #include <stdio.h>
 
-status_t get_algo_args(algorithm_t *alg) {
-  if (alg->additional_args == NULL)
-    return status_ok();
-
-  pargument_t cur = alg->additional_args;
-  argument_union_t arg_union;
-  status_t valid_args;
-  int user_int;
-  float user_flt;
-
-  while (cur != NULL) {
-    switch (cur->arg_type) {
-    case INTEGER_ARG:
-
-      do {
-        printf("\nPlease input a value for %s (integer, default: %d)\n",
-               cur->description, cur->arg_union.integer);
-        status_t get_int_status = get_user_int(&user_int);
-
-        if (!get_int_status.success) {
-          fprintf(
-              stderr, "Error: %s\nUsing default arg.\n",
-              get_int_status.message); // Currently this essentailly ruins the
-                                       // entire algorithm if it happens :p
-        }
-
-        valid_args = alg->validate_args(alg->additional_args);
-      } while (!valid_args.success);
-
-      arg_union = (argument_union_t){ .integer = user_int };
-      cur->arg_union = arg_union;
-      break;
-
-    case STRING_ARG:
-
-      printf("\nPlease input a value for %s (string, default: %s)\n",
-             cur->description, cur->arg_union.string);
-      result_t get_str_result = get_user_string();
-
-      if (!get_str_result.success) {
-        fprintf(stderr, "Error: %s", get_str_result.message);
-        return status_error("Error when getting user input");
-      }
-      arg_union = (argument_union_t){.string = get_str_result.data};
-      cur->arg_union = arg_union;
-      break;
-
-    case FLOAT_ARG:
-
-      printf("\nPlease input a value for %s (float, default: %f)\n",
-             cur->description, cur->arg_union.fp);
-      status_t get_flt_status = get_user_flt(&user_flt);
-
-      if (!get_flt_status.success) {
-        fprintf(stderr, "Error: %s", get_flt_status.message);
-        return status_error("Error when getting user input");
-      }
-      arg_union = (argument_union_t){.fp = user_flt};
-      cur->arg_union = arg_union;
-      break;
+status_t get_algo_args(algorithm_t* alg) {
+    if (alg->additional_args == NULL) {
+        return status_ok();
     }
 
-    cur = cur->next;
-  }
+    pargument_t cur = alg->additional_args;
+    while (1) {
+        switch (cur->arg_type) {
+        case INTEGER_ARG:
+            printf("\nPlease input a value for %s (integer, default: %d)\n", cur->description, cur->arg_union.integer);
 
-  return status_ok();
+            int user_int;
+            status_t get_int_status = get_user_int(&user_int);
+            if (!get_int_status.success) {
+                fprintf(stderr, "Error: %s\nUsing default arg.\n", get_int_status.message);
+                break;
+            }
+
+            cur->arg_union = (argument_union_t) { .integer = user_int };
+            break;
+
+        case STRING_ARG:
+            printf("\nPlease input a value for %s (string, default: %s)\n", cur->description, cur->arg_union.string);
+
+            result_t get_str_result = get_user_string();
+            if (!get_str_result.success) {
+                fprintf(stderr, "Error: %s", get_str_result.message);
+                return status_error("Error when getting user input");
+            }
+
+            cur->arg_union = (argument_union_t) { .string = get_str_result.data };
+            break;
+
+        case FLOAT_ARG:
+            printf("\nPlease input a value for %s (float, default: %.3f)\n", cur->description, cur->arg_union.fp);
+
+            float user_flt;
+            status_t get_flt_status = get_user_flt(&user_flt);
+            if (!get_flt_status.success) {
+                fprintf(stderr, "Error: %s\nUsing default arg.\n", get_flt_status.message);
+                break;
+            }
+
+            cur->arg_union = (argument_union_t) { .fp = user_flt };
+            break;
+        }
+
+        cur = cur->next;
+        if (!cur) {
+            status_t validation_stat = alg->validate_args(alg->additional_args);
+            if (!validation_stat.success) {
+                fprintf(stderr, "Error: %s\nPlease re-enter the arguments.\n", validation_stat.message);
+                
+                // Reset arguments to default value
+                alg->reset_args(alg);
+
+                // Reset iterator to first arg
+                cur = alg->additional_args;
+
+                continue;
+            }
+
+            break;
+        }
+    }
+
+    return status_ok();
 }
