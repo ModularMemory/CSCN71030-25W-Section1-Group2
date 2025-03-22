09eb858a85e277967fef582aa1d712b7aaa93f15
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2025-03-22 01:48:15 -0400

    Add a few tests

--- CiphCzar.Tests/recipe_enumerator.tests.cpp
+++ CiphCzar.Tests/recipe_enumerator.tests.cpp
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "CppUnitTest.h"
+#include "crtdbg.h"
 
 extern "C" {
     #include "../CiphCzar/algorithm.h"
@@ -41,16 +42,84 @@ namespace CiphCzarTests
             return rec;
         }
 
+        static recipe_t create_empty_recipe() {
+            result_t res = create_recipe();
+            Assert::IsTrue(res.success);
+            Assert::IsNotNull(res.data);
+
+            recipe_t rec = (recipe_t)res.data;
+
+            return rec;
+        }
+
         static recipe_enumerator_t create_test_enumerator(recipe_t recipe, data_t data) {
             result_t res = create_recipe_enumerator(recipe, data);
             Assert::IsTrue(res.success);
             Assert::IsNotNull(res.data);
-            
+
             return (recipe_enumerator_t)res.data;
         }
 
     public:
-        TEST_METHOD(Enumerator_Execute_Produces_Ouput)
+        TEST_METHOD(MoveNext_ReturnsTrue_ForRecipeCount)
+        {
+            // Arrange
+            recipe_t recipe = create_test_recipe();
+            size_t recipe_count = get_recipe_count(recipe);
+            data_t test_data = create_test_data("ABC123 foo");
+            recipe_enumerator_t enumerator = create_test_enumerator(recipe, test_data);
+
+            for (size_t i = 0; i < recipe_count; i++) {
+                Assert::IsTrue(recipe_enumerator_move_next(enumerator));
+            }
+
+            Assert::IsFalse(recipe_enumerator_move_next(enumerator));
+
+            // Cleanup
+            destroy_recipe(recipe);
+            free(test_data.data);
+            destroy_recipe_enumerator(enumerator);
+        }
+
+        TEST_METHOD(MoveNext_ReturnsFalse_WhenEmpty)
+        {
+            // Arrange
+            recipe_t recipe = create_empty_recipe();
+            data_t test_data = create_test_data("ABC123 foo");
+            recipe_enumerator_t enumerator = create_test_enumerator(recipe, test_data);
+
+            // Act
+            bool actual = recipe_enumerator_move_next(enumerator);
+
+            // Assert
+            Assert::IsFalse(actual);
+
+            // Cleanup
+            destroy_recipe(recipe);
+            free(test_data.data);
+            destroy_recipe_enumerator(enumerator);
+        }
+
+        TEST_METHOD(Execute_ReturnsError_WhenUnmoved)
+        {
+            // Arrange
+            recipe_t recipe = create_test_recipe();
+            data_t test_data = create_test_data("ABC123 foo");
+            recipe_enumerator_t enumerator = create_test_enumerator(recipe, test_data);
+
+            // Act
+            status_t actual = recipe_enumerator_execute(enumerator);
+
+            // Assert
+            Assert::IsFalse(actual.success);
+
+            // Cleanup
+            destroy_recipe(recipe);
+            free(test_data.data);
+            destroy_recipe_enumerator(enumerator);
+        }
+
+        TEST_METHOD(Execute_Produces_Ouput)
         {
             // Arrange
             recipe_t recipe = create_test_recipe();
@@ -75,8 +144,33 @@ namespace CiphCzarTests
             }
 
             // Cleanup
+            destroy_recipe(recipe);
             free(test_data.data);
             destroy_recipe_enumerator(enumerator);
+        }
+
+        TEST_METHOD(Destroy_FreesHeldMemory)
+        {
+            // Arrange
+            recipe_t recipe = create_empty_recipe();
+            data_t test_data = create_test_data("ABC123 foo");
+            recipe_enumerator_t enumerator = create_test_enumerator(recipe, test_data);
+
+            // Act
+            _CrtMemState before = {0};
+            _CrtMemDumpStatistics(&before);
+
+            destroy_recipe_enumerator(enumerator);
+
+            _CrtMemState after = {0};
+            _CrtMemDumpStatistics(&after);
+
+            // Assert
+            Assert::IsTrue(after.lCounts < before.lCounts);
+
+            // Cleanup
+            destroy_recipe(recipe);
+            free(test_data.data);
         }
     };
 }
